48134872c463c6c4a6149b76c12821046df42bea
Elton Fonseca <elton869@gmail.com>
2021-07-03 16:44:06 -0300

    03 - Realizando validação usando método make

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CadastroController extends Controller
 {
     public function store()
     {
+        $validacao = Validator::make(
+            [
+                "curso" => "PHP",
+                
+            ],
+            [
+                "curso" => ['required', 'max:100'],
+                "carga" => ['required', 'integer']
+            ]
+        );
+
+        if($validacao->fails()) {
+            dd('a validação falhou!');
+        }
+
         dd('cheguei aqui');
     }
 }
