9726985e3bc9f5becc0cde8926edcc346abe57c5
Elton Fonseca <elton869@gmail.com>
2021-07-03 18:27:23 -0300

    07 - Usando Form Request

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CursoRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class CadastroController extends Controller
 {
-    public function store(Request $request)
+    public function store(CursoRequest $request)
     {
-        $request->validate([
-            "curso" => ['required', 'max:100'],
-            "carga" => ['required', 'integer']
-        ]);
+        // $request->validate([
+        //     "curso" => ['required', 'max:100'],
+        //     "carga" => ['required', 'integer']
+        // ]);
 
         // Validator::make($request->except('_token'),
         //     [
@@ -21,6 +22,6 @@ class CadastroController extends Controller
         //     ]
         // )->validate();
 
-        dd('cheguei aqui');
+        dd($request->all());
     }
 }
